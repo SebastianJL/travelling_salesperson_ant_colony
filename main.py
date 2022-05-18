313c3fe4f72486b96cf095829d3e481cc1e72d68
Casper <camenisch.casper@outlook.com>
2022-05-18 10:40:44 +0200

    Fixed input reading and adjusted plotting.

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ def read_problem_input(file_name: str) -> List[Tuple[float, float]]:
     with open(file_name, "r") as f:
         for line in f:
             line = line.strip()
+            if line == "":
+                continue
             if line[0].isdigit():
                 t, x, y = line.split()[0:3]
                 cities.append((float(x.strip()), float(y.strip())))
@@ -21,7 +23,7 @@ def read_solution_input(file_name: str) -> List[int]:
     with open(file_name, "r") as f:
         for line in f:
             if line[0].isdigit():
-                solution_path.append(int(line.strip()))
+                solution_path.append(int(line.strip())-1)
     return solution_path
 
 
@@ -115,19 +117,20 @@ def plot_path(cities: List[Tuple[float, float]], path: List[int], ax=None):
     ys = [c[1] for c in cities]
     ax.scatter(xs, ys, c="k")
     for i in range(len(path)):
-        x1, y1 = cities[path[i] - 1]
-        x2, y2 = cities[path[(i + 1)%len(path)] - 1]
+        x1, y1 = cities[path[i]]
+        x2, y2 = cities[path[(i + 1)%len(path)]]
         ax.plot([x1, x2], [y1, y2], c="r")
+    ax.axis("equal")
 
 
 def main():
     cities: List[Tuple[float, float]]
     proposed_path: List[int]
-    cities_file = "tsp_problems/burma14.tsp"
-    #proposed_path_file = "tsp_problems/berlin52.opt.tour"
+    cities_file = "tsp_problems/berlin52.tsp"
+    proposed_path_file = "tsp_problems/berlin52.opt.tour"
     cities = read_problem_input(cities_file)
-    print(cities)
-    #proposed_path = read_solution_input(proposed_path_file)
+    proposed_path = read_solution_input(proposed_path_file)
+    #proposed_path = list(range(14))
 
     max_iterations = 100
     n_ants = len(cities)
@@ -135,7 +138,7 @@ def main():
 
     fig, (ax1, ax2) = plt.subplots(1, 2)
     plot_path(cities, shortest_path, ax=ax1)
-    #plot_path(cities, proposed_path, ax=ax2)
+    plot_path(cities, proposed_path, ax=ax2)
     plt.show()
 
 
